ac1dcac24b6061dbcec92bf5bdb63d8e7187e32f
Remi Krommenaker <remi.krommenaker@gmail.com>
2022-02-07 11:41:37 +0100

    Add a frontend music player and created local server to avoid CORS issues when requesting data from the spotify API

---
+++ music_player/script.js
@@ -0,0 +1,144 @@
+let currentTrack = {
+  trackName: "",
+  artist: "",
+  duration: 0,
+  progression: 0,
+  urlCover: ""
+};
+
+const trackName = document.getElementById("track-name")
+const trackArtist = document.getElementById("track-artist")
+const trackDuration = document.getElementById("track-duration")
+const trackProgression = document.getElementById("track-progression")
+const trackCoverUrl = document.getElementById("track-url-cover")
+
+
+
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+myHeaders.append("Authorization", "Basic NGI3YzU0MzZlMjgwNDM0Zjg0NGVkYzE3NGVmMzNlNmE6NmMxNzM0OTJhODgxNGJlYzg0YWI4ZDFjNDJjNTlkZWU=");
+myHeaders.append("Cookie", "__Host-device_id=AQBjxVEe2baK33vxa608ibFQ_6uNKMae8rrsaAiENGBrFqJymFrj06ys0WqDQ4hgXjWwLSIXC8zG4mA3-qANtmzWxiix_6D7Fjg; sp_aor=React; sp_tr=false");
+
+let urlencoded = new URLSearchParams();
+urlencoded.append("grant_type", "refresh_token");
+urlencoded.append("refresh_token", "AQAkmHktN9YJP5YwHOCo1rJcw8xyR6yn6LOCTywf6Gso9eFG99CwjF2_B7Yu4Udb5BsUIrI--QjDhAG6rm62vUwcX-grmEguFnQ-_vjaY474tIJ7YzpdlToeyUNURfSo95I");
+urlencoded.append("client_id", "4b7c5436e280434f844edc174ef33e6a");
+
+let requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: 'follow'
+};
+
+let myHeadersForCurrentTrack = new Headers();
+myHeadersForCurrentTrack.append("Access-Control-Allow-Origin", "*")
+
+
+
+async function getAccessToken() {
+  // console.log("calling")
+  await fetch("https://accounts.spotify.com/api/token", requestOptions)
+      .then(response => response.json())
+      .then(data => bearerToken = data["access_token"])
+      .catch(error => console.log('error', error));
+  console.log(bearerToken)
+}
+
+
+let requestOptionsForCurrentTrack = {
+  method: 'GET',
+  headers: myHeadersForCurrentTrack,
+  redirect: 'follow',
+};
+let bearerToken;
+
+async function getCurrentlyPlaying(){
+  myHeadersForCurrentTrack.append("Authorization", `Bearer ${bearerToken}` );
+  await fetch("https://api.spotify.com/v1/me/player/currently-playing", requestOptionsForCurrentTrack)
+      .then(response => {console.log(response.text)
+        response.json()
+      })
+      .then(result => currentTrack = {...currentTrack,
+        trackName: result["item"]["name"],
+        artist: result["item"]["artists"][0]["name"].slice(0, 30),
+        duration: Math.round(result["item"]["duration_ms"] / 1000),
+        progression: Math.round(result["progress_ms"] / 1000),
+        urlCover: result["item"]["album"]["images"][0]["url"]
+      } )
+      .catch(error => console.log('error', error));
+
+}
+
+async function getCurrentlyPlayingTrackFromPython(){
+  let myHeadersForCurrentTrack = new Headers();
+  myHeadersForCurrentTrack.append("token", bearerToken)
+  let requestOptionsForCurrentTrack = {
+  method: 'GET',
+  headers: myHeadersForCurrentTrack,
+  redirect: 'follow',
+};
+  await fetch("http://localhost:8080/currently_playing", requestOptionsForCurrentTrack, )
+      .then(response =>
+          response.json()
+      )
+      .then(result => {console.log(result);currentTrack = {...currentTrack,
+        trackName: result["item"]["name"],
+        artist: result["item"]["artists"][0]["name"].slice(0, 30),
+        duration: Math.round(result["item"]["duration_ms"] / 1000),
+        progression: Math.round(result["progress_ms"] / 1000),
+        urlCover: result["item"]["album"]["images"][0]["url"]}} )
+      .catch(error => console.log('error', error));
+}
+
+function displayInformation(){
+  trackName.innerText = currentTrack.trackName
+  if ((currentTrack.trackName.length) > 30) {
+    trackName.innerText = currentTrack.trackName.slice(0, 30) + "..."}
+  trackArtist.innerText = currentTrack.artist
+  trackCoverUrl.src = currentTrack.urlCover
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+getAccessToken()
+
+setInterval(function () {
+      getAccessToken()
+    },
+    50*60*1000);
+
+
+
+var i = 0;
+function move() {
+  if (i === 0) {
+    i = 1;
+    var progressBarInHtml = document.getElementById("bar");
+    // le debut de la largeur de la barre
+    var progressBarInPercent = currentTrack.progression / currentTrack.duration * 100;
+    var id = setInterval(frame, 6000);
+    function frame() {
+      progressBarInPercent = currentTrack.progression / currentTrack.duration * 100;
+      progressBarInHtml.style.width = progressBarInPercent + "%";
+    }
+  }
+}
+move()
+
+async function mainLoop() {
+  // Sleep in loop
+  while ("a" === "a") {
+    // await getCurrentlyPlaying()
+    await getCurrentlyPlayingTrackFromPython()
+    displayInformation()
+    await sleep(6000);
+  }
+
+}
+
+setTimeout(function (){
+  mainLoop()
+}, 3000);
--- spotify/spotify.py
+++ spotify/spotify.py
@@ -3,6 +3,9 @@ import requests
 from dotenv import load_dotenv
 import os
 import playsound
+from bottle import route, run, static_file, request
+from datetime import datetime
+from threading import Thread
 
 load_dotenv()
 
@@ -11,57 +14,53 @@ BASIC64_AUTHORIZATION = os.getenv("BASIC64_AUTHORIZATION")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+ACCESS_TOKEN = None
+LAST_TIME_REFRESH = None
 
 
-def access_token_and_refresh_token():
-    url = "https://accounts.spotify.com/api/token"
-    payload = (
-        f"grant_type=authorization_code&code="
-        f"{SPOTIFY_CODE}&redirect_uri={REDIRECT_URI}"
-    )
-    headers = {
-        # Base 64 encoded string that contains the client ID and client secret key of spotify app.
-        # The field must have the format:
-        # Authorization: Basic <base64 encoded client_id:client_secret>
-        "Authorization": f"{BASIC64_AUTHORIZATION}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
+def refreshed_token(is_init=True):
+    global ACCESS_TOKEN
+    global LAST_TIME_REFRESH
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    response = response.json()
+    if is_init:
+        LAST_TIME_REFRESH = datetime.now()
+        url = "https://accounts.spotify.com/api/token"
 
-    access_token = response["access_token"]
-    refresh_token = response["refresh_token"]
+        payload = f'grant_type=refresh_token&refresh_token={REFRESH_TOKEN}&client_id={SPOTIFY_CLIENT_ID}'
+        headers = {
+            'Authorization': f'{BASIC64_AUTHORIZATION}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-    return access_token, refresh_token
-
-
-def refreshed_token():
-    url = "https://accounts.spotify.com/api/token"
-
-    payload = f"grant_type=refresh_token&refresh_token={REFRESH_TOKEN}&client_id={SPOTIFY_CLIENT_ID}"
-    headers = {
-        "Authorization": f"{BASIC64_AUTHORIZATION}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-    response = response.json()
-    access_token = response["access_token"]
-    return access_token
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response = response.json()
+        ACCESS_TOKEN = response["access_token"]
+        return ACCESS_TOKEN
+    else:
+        actual_time = datetime.now()
+        if (LAST_TIME_REFRESH - actual_time).seconds > 3500:
+            new_token = refreshed_token(True)
+            return new_token
+        else:
+            return ACCESS_TOKEN
 
 
-def get_track_uri(name, access_token):
+refreshed_token(True)
+
+
+def get_track_uri(name):
     """take the name of a song and return its uri"""
     url = f"https://api.spotify.com/v1/search?q={name}&type=track"
 
     payload = {}
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = {"Authorization": f"Bearer {refreshed_token()}"}
 
     response = requests.request("GET", url, headers=headers, data=payload)
     response_json = response.json()
     song_found = response_json["tracks"]["items"]
     if not song_found:
+        # play a funny sound if the song is not found
+        playsound.playsound("./media/pas_trouve.mp3")
         print("***   No Song found   ***")
     else:
         song_name = response_json["tracks"]["items"][0]["name"]
@@ -80,7 +79,7 @@ def skip_current_track():
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-    playsound.playsound("./media/dj-scratch.mp3")
+    playsound.playsound("./media/ca_degage.mp3")
 
 
 def add_track_to_playback(song_uri):
@@ -94,5 +93,35 @@ def add_track_to_playback(song_uri):
 
 def play_music_requested(user_input):
     add_track_to_playback(
-        get_track_uri(user_input, refreshed_token()),
+
+        get_track_uri(user_input),
     )
+
+
+@route('/currently_playing')
+def get_currently_played():
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {request.get_header("token")}',
+        'Access-Control-allow-Origin': '*'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response = response.text
+    return json.loads(response)
+
+
+
+@route('/static/<filename>')
+def get_static(filename):
+    return static_file(filename, root='music_player')
+
+
+def run_server():
+    run(host='localhost', port=8080)
+
+
+Thread(target=run_server).start()
